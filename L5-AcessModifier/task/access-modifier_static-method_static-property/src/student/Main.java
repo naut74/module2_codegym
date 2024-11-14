386e01f06755b3e2e680d2fc589f5db77c6a8541
naut74 <ngoctuan14032001@gmail.com>
2024-11-14 11:11:58 +0700

    update

---
+++ L5-AcessModifier/task/access-modifier_static-method_static-property/src/student/Main.java
@@ -0,0 +1,9 @@
+package student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println("Name: " + student1.getName());
+        System.out.println("Classes: " + student1.getClasses());
+    }
+}
---
+++ L5-AcessModifier/task/access-modifier_static-method_static-property/src/student/Student.java
@@ -0,0 +1,24 @@
+package student;
+
+public class Student {
+    private String name = "Jonh";
+    private String classes = "CO2";
+
+    public Student() {};
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+
+}
