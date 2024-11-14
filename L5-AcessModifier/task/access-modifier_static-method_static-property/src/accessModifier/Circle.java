4d92422b4c1a04b52a9f0525f6da3aa840101b78
naut74 <ngoctuan14032001@gmail.com>
2024-11-14 10:31:59 +0700

    first commit

---
+++ L5-AcessModifier/task/access-modifier_static-method_static-property/src/accessModifier/Circle.java
@@ -0,0 +1,20 @@
+package accessModifier;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {};
+
+    public Circle(double r) {
+        this.radius = r;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
---
+++ L5-AcessModifier/task/access-modifier_static-method_static-property/src/accessModifier/Main.java
@@ -0,0 +1,14 @@
+package accessModifier;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        System.out.println("Radius: " + circle1.getRadius());
+        System.out.println("Area: " + circle1.getArea());
+
+        Circle circle2 = new Circle(25);
+        System.out.println("Radius: " + circle2.getRadius());
+        System.out.println("Area: " + circle2.getArea());
+
+    }
+}
