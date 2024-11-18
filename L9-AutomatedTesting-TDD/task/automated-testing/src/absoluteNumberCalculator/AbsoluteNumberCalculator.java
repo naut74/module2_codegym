9f6db01583345e910472d6a784796030b0ae4590
naut74 <ngoctuan14032001@gmail.com>
2024-11-18 16:27:07 +0700

    absolute number

---
+++ L9-AutomatedTesting-TDD/task/automated-testing/src/absoluteNumberCalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,8 @@
+package absoluteNumberCalculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number) {
+        return number < 0 ? -number : number;
+    }
+}
+
---
+++ L9-AutomatedTesting-TDD/task/automated-testing/src/absoluteNumberCalculator/test/absoluteNumberCalculator/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,44 @@
+package absoluteNumberCalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    void findAbsolute1() {
+        int number = 0;
+        int expected = 0;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findAbsolute2() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findAbsolute3() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findAbsolute4() {
+        int number = -20;
+        int expected = 20;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+}
