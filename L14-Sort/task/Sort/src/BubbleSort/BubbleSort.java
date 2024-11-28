5d6c896afae650ec3dba5635dc616be3927a28bf
naut74 <ngoctuan14032001@gmail.com>
2024-11-28 13:35:41 +0700

    BubbleSort

---
+++ L14-Sort/task/Sort/src/BubbleSort/BubbleSort.java
@@ -0,0 +1,29 @@
+package BubbleSort;
+
+public class BubbleSort {
+    static int[] list = {2, 3, 4, 5, 23, -2, 67, 12, 25, 99};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int k = 0; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + " ");
+        }
+    }
+}
