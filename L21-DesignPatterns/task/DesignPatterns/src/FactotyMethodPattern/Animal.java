3fd83ac7d96164ea1ca4739086715b6cf49d4e32
naut74 <ngoctuan14032001@gmail.com>
2024-12-08 00:36:18 +0700

    factory method demo

---
+++ L21-DesignPatterns/task/DesignPatterns/src/FactotyMethodPattern/Animal.java
@@ -0,0 +1,5 @@
+package FactotyMethodPattern;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ L21-DesignPatterns/task/DesignPatterns/src/FactotyMethodPattern/Dog.java
@@ -0,0 +1,8 @@
+package FactotyMethodPattern;
+
+public class Dog extends Animal{
+    @Override
+    public String makeSound() {
+        return "woof";
+    }
+}
